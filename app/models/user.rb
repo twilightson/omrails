935c83fca06ee9546126a477c73a9f4bf909cd58
Sal Matteis <s.mattteis@yahoo.com>
2013-07-22 16:38:33 +0200

    Add Pins Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
   :rememberable, :trackable, :validatable
   # attr_accessible :title, :body
 attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :provider, :uid, :as => [:default, :admin]
+ has_many :pins, :dependent => :destroy
 end
