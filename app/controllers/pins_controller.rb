0eb0a0a83d3188b2ef22c55c40f5039bb59d1c25
Sal Matteis <s.mattteis@yahoo.com>
2013-07-26 15:21:21 +0200

    Add Pagination

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ before_filter :authenticate_user!, except: [:index]
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.order("created_at desc")
+    @pins = Pin.order("created_at desc").page(params[:page]).per_page(20)
     
     respond_to do |format|
       format.html # index.html.erb
